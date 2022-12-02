f6f579d9b016e3d39418dc2ecb35b7815a39a743
Jakub <jakeic@st.amu.edu.pl>
2022-12-02 18:43:41 +0100

    Day2 - Rock Paper Scissors

---
+++ Day2/dist/app.js
@@ -0,0 +1,25 @@
+import fs from 'fs';
+//Load data
+const input = fs.readFileSync('./input.txt', 'utf-8').split('\r\n');
+const game = { 'A': 1, 'B': 2, 'C': 3, 'X': 1, 'Y': 2, 'Z': 3 };
+const test = `A Y
+B X
+C Z`.split('\n');
+//Part One
+let sum1 = 0;
+input.forEach(e => {
+    sum1 += game[e[2]];
+    //game value difference 
+    //+3 to avoid negatives
+    sum1 += ((game[e[2]] - game[e[0]] + 4) % 3) * 3;
+});
+console.log(sum1);
+//Part Two
+let sum2 = 0;
+input.forEach(e => {
+    sum2 += (game[e[2]] - 1) * 3;
+    const pom = (game[e[0]] + game[e[2]] + 1) % 3;
+    sum2 += pom > 0 ? pom : 3;
+});
+console.log(sum2);
+//# sourceMappingURL=app.js.map
